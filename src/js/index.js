21355eca08cd14aa6ca078e21dfc1e84fce164b1
Michel Rivero <157402536+michelriv@users.noreply.github.com>
2024-03-12 00:55:25 +0000

    finished

--- src/js/index.js
+++ src/js/index.js
@@ -1,12 +1,52 @@
 //import react into the bundle
 import React from "react";
 import ReactDOM from "react-dom";
-
+import PropTypes from "prop-types";
 // include your styles into the webpack bundle
 import "../styles/index.css";
 
-//import your own components
-import Home from "./component/home.jsx";
+function SimpleCounter(props){
+    return (<div className="bigCounter">
+        <div className="calendar">
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-stopwatch" viewBox="0 0 16 16">
+    <path d="M8.5 5.6a.5.5 0 1 0-1 0v2.9h-3a.5.5 0 0 0 0 1H8a.5.5 0 0 0 .5-.5V5.6z"/>
+    <path d="M6.5 1A.5.5 0 0 1 7 .5h2a.5.5 0 0 1 0 1v.57c1.36.196 2.594.78 3.584 1.64a.715.715 0 0 1 .012-.013l.354-.354-.354-.353a.5.5 0 0 1 .707-.708l1.414 1.415a.5.5 0 1 1-.707.707l-.353-.354-.354.354a.512.512 0 0 1-.013.012A7 7 0 1 1 7 2.071V1.5a.5.5 0 0 1-.5-.5zM8 3a6 6 0 1 0 .001 12A6 6 0 0 0 8 3z"/>
+</svg>
+        </div>
+        <div className="four">{props.digitFour}0 </div>
+        <div className="three">{props.digitThree}0 </div>
+        <div className="two">{props.digitTwo} 0 </div>
+        <div className="one">{props.digitOne}0 </div>
+        
+    </div>);
+}
+SimpleCounter.propTypes = {
+    digitFour: PropTypes.number,
+    digitThree: PropTypes.number,
+    digitTwo: PropTypes.number,
+    digitOne: PropTypes.number,
+    
+};
 
-//render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+
+let counter = 0;
+setInterval(function(){
+    const four = Math.floor(counter/1000);
+    const three = Math.floor(counter/100);
+    const two = Math.floor(counter/10);
+    const one = Math.floor(counter/1);
+    console.log(four, three, two, one);
+
+
+    counter ++;
+    ReactDOM.render(
+        <SimpleCounter digitOne={counter.index.Of()} />,
+        document.querySelector('#app')
+
+    );
+},1000);
+
+ReactDOM.render(
+    <SimpleCounter />,
+    document.querySelector("#app")
+)
